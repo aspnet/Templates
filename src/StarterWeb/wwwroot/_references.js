22e915a80e397790145eab16ee4d5550ca7d5a7b
Pranav Rastogi <raspranav@gmail.com>
2015-01-26 10:34:27 -0800

    Carousel

--- src/StarterWeb/wwwroot/_references.js
+++ src/StarterWeb/wwwroot/_references.js
@@ -4,3 +4,5 @@
 /// <reference path="lib/jquery-validation/jquery.validate.js" />
 /// <reference path="lib/jquery/jquery.js" />
 /// <reference path="lib/bootstrap/js/bootstrap.js" />
+/// <reference path="lib/hammer.js/hammer.js" />
+/// <reference path="lib/bootstrap-touch-carousel/js/bootstrap-touch-carousel.js" />
