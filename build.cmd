4367ebba9f2fefcc49848c4885508e545e67af75
N. Taylor Mullen <nimullen@microsoft.com>
2015-03-08 13:00:09 -0700

    Update kvm/KVM/Kvm => dnvm/DNVM/Dnvm.

--- build.cmd
+++ build.cmd
@@ -30,9 +30,9 @@ IF EXIST packages\KoreBuild goto run
 .nuget\NuGet.exe install Sake -version 0.2 -o packages -ExcludeVersion
  
 IF "%SKIP_DNX_INSTALL%"=="1" goto run 
-CALL packages\KoreBuild\build\kvm upgrade -runtime CLR -x86 
-CALL packages\KoreBuild\build\kvm install default -runtime CoreCLR -x86 
+CALL packages\KoreBuild\build\dnvm upgrade -runtime CLR -x86 
+CALL packages\KoreBuild\build\dnvm install default -runtime CoreCLR -x86 
  
 :run 
-CALL packages\KoreBuild\build\kvm use default -runtime CLR -x86
+CALL packages\KoreBuild\build\dnvm use default -runtime CLR -x86
 CALL tools\BuildAndVerify.cmd /t:%SCRIPTARGS%
