3b292549c0b2505481bd047a84a9aef0a9b6d737
Phil Henning <phenning@microsoft.com>
2015-03-20 09:44:35 -0700

    React to Identity PrincipalExtensions Changes

--- src/Rules/StarterWeb/IndividualAuth/Controllers/AccountController.cs
+++ src/Rules/StarterWeb/IndividualAuth/Controllers/AccountController.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Principal;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Authorization;
 using Microsoft.AspNet.Identity;
