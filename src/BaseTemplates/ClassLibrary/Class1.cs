f0478ae01d027b28c52568d8209b6d05523142ec
joeloff <joeloff@microsoft.com>
2015-04-10 20:27:27 -0700

    Add comment to class1.cs on how to produce a package

--- src/BaseTemplates/ClassLibrary/Class1.cs
+++ src/BaseTemplates/ClassLibrary/Class1.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 
 namespace $safeprojectname$
 {
+    // This project can output the Class library as a Package.
+    // To enable this option, right-click the project to open the properties window. In the Build tab select "Product outputs on builds".
     public class Class1
     {
         public Class1()
