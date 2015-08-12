b6c2ba54811ec129a4ea288917f6c76a80eff50e
Phil Henning <phil.henning@live.com>
2015-08-12 16:08:04 -0700

    Reorganize templates in accordance with usual standards
    * Move ViewModels into their own folder (subfolders for Account and Manage)
    * Put each type/class in its own file
    * Removed unused using statements
     - Except those which are required by commented code, and the always-four

--- src/Rules/WebAPI/AI/NoAuth/Startup.cs
+++ src/Rules/WebAPI/AI/NoAuth/Startup.cs
@@ -4,8 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
-using Microsoft.AspNet.Http;
-using Microsoft.AspNet.Routing;
 using Microsoft.Dnx.Runtime;
 using Microsoft.Framework.Configuration;
 using Microsoft.Framework.DependencyInjection;
