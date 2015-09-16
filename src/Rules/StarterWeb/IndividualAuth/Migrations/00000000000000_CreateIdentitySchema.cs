d1661e286307bbe3b0897ed357ee63674611939e
Phil Henning <phil.henning@live.com>
2015-09-15 17:08:28 -0700

    React to EF namespace changes

--- src/Rules/StarterWeb/IndividualAuth/Migrations/00000000000000_CreateIdentitySchema.cs
+++ src/Rules/StarterWeb/IndividualAuth/Migrations/00000000000000_CreateIdentitySchema.cs
@@ -2,8 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Data.Entity.Metadata;
 using Microsoft.Data.Entity.Migrations;
-using Microsoft.Data.Entity.SqlServer.Metadata;
 
 namespace $safeprojectname$.Migrations
 {
