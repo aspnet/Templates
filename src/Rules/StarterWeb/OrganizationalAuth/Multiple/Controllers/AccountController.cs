246d14e133ee14aa58cddc83ab47da4255cf32df
Phil Henning <phil.henning@live.com>
2015-09-28 10:22:58 -0700

    React to Http Context property rename

--- src/Rules/StarterWeb/OrganizationalAuth/Multiple/Controllers/AccountController.cs
+++ src/Rules/StarterWeb/OrganizationalAuth/Multiple/Controllers/AccountController.cs
@@ -20,15 +20,15 @@ namespace $safeprojectname$.Controllers
         public async Task<IActionResult> SignOut()
         {
             var callbackUrl = Url.Action("SignOutCallback", "Account", values: null, protocol: Request.Scheme);
-            await Context.Authentication.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-            await Context.Authentication.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme,
+            await HttpContext.Authentication.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            await HttpContext.Authentication.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme,
                 new AuthenticationProperties { RedirectUri = callbackUrl });
             return new EmptyResult();
         }
 
         public IActionResult SignOutCallback()
         {
-            if (Context.User.Identity.IsAuthenticated)
+            if (HttpContext.User.Identity.IsAuthenticated)
             {
                 // Redirect to home page if the user is authenticated.
                 return RedirectToAction(nameof(HomeController.Index), "Home");
