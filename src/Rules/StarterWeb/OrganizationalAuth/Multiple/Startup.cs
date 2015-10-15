844bdb010b64558d2affabf20f2ed42293ad9e3e
Hao Kung <haok@microsoft.com>
2015-10-14 17:12:21 -0700

    React to Auth changes

--- src/Rules/StarterWeb/OrganizationalAuth/Multiple/Startup.cs
+++ src/Rules/StarterWeb/OrganizationalAuth/Multiple/Startup.cs
@@ -40,11 +40,6 @@ namespace $safeprojectname$
         // This method gets called by the runtime.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookieAuthenticationOptions>(options =>
-            {
-                options.AutomaticAuthentication = true;
-            });
-
             // Add MVC services to the services container.
             services.AddMvc();
         }
@@ -78,12 +73,12 @@ namespace $safeprojectname$
             app.UseStaticFiles();
 
             // Add cookie-based authentication to the request pipeline.
-            app.UseCookieAuthentication();
+            app.UseCookieAuthentication(options => options.AutomaticAuthentication = true);
 
             // Add OpenIdConnect middleware so you can login using Azure AD.
             app.UseOpenIdConnectAuthentication(options =>
             {
-                options.AutomaticAuthentication = true;
+                options.AutomaticChallenge = true;
                 options.ClientId = Configuration["Authentication:AzureAd:ClientId"];
                 options.Authority = Configuration["Authentication:AzureAd:AADInstance"] + "Common";
                 options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
