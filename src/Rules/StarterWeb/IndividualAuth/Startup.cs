052c48322f777c7db9acaf6c2d7eea75c7bb8c5f
Thom Kiesewetter <thom@kiesewetter.nl>
2015-10-29 11:13:12 +0100

    Restore layout

--- src/Rules/StarterWeb/IndividualAuth/Startup.cs
+++ src/Rules/StarterWeb/IndividualAuth/Startup.cs
@@ -101,41 +101,41 @@ namespace $safeprojectname$
             // Add cookie-based authentication to the request pipeline.
             app.UseIdentity();
 
-        // Add and configure the options for authentication middleware to the request pipeline.
-        // You can add options for middleware as shown below.
-        // For more information see http://go.microsoft.com/fwlink/?LinkID=532715
-        //app.UseFacebookAuthentication(options =>
-        //{
-        //    options.AppId = Configuration["Authentication:Facebook:AppId"];
-        //    options.AppSecret = Configuration["Authentication:Facebook:AppSecret"];
-        //});
-        //app.UseGoogleAuthentication(options =>
-        //{
-        //    options.ClientId = Configuration["Authentication:Google:ClientId"];
-        //    options.ClientSecret = Configuration["Authentication:Google:ClientSecret"];
-        //    options.Events = new OAuthEvents()
-        //    {
-        //        OnRemoteError = ctx =>
-        //        {
-        //            ctx.Response.Redirect("/Account/ExternalLoginCallback?remoteError=" + UrlEncoder.Default.UrlEncode(ctx.Error.Message));
-        //            ctx.HandleResponse();
-        //            return Task.FromResult(0);
-        //        }
-        //    };
-        //});
-        //app.UseMicrosoftAccountAuthentication(options =>
-        //{
-        //    options.ClientId = Configuration["Authentication:MicrosoftAccount:ClientId"];
-        //    options.ClientSecret = Configuration["Authentication:MicrosoftAccount:ClientSecret"];
-        //});
-        //app.UseTwitterAuthentication(options =>
-        //{
-        //    options.ConsumerKey = Configuration["Authentication:Twitter:ConsumerKey"];
-        //    options.ConsumerSecret = Configuration["Authentication:Twitter:ConsumerSecret"];
-        //});
+            // Add and configure the options for authentication middleware to the request pipeline.
+            // You can add options for middleware as shown below.
+            // For more information see http://go.microsoft.com/fwlink/?LinkID=532715
+            //app.UseFacebookAuthentication(options =>
+            //{
+            //    options.AppId = Configuration["Authentication:Facebook:AppId"];
+            //    options.AppSecret = Configuration["Authentication:Facebook:AppSecret"];
+            //});
+            //app.UseGoogleAuthentication(options =>
+            //{
+            //    options.ClientId = Configuration["Authentication:Google:ClientId"];
+            //    options.ClientSecret = Configuration["Authentication:Google:ClientSecret"];
+            //    options.Events = new OAuthEvents()
+            //    {
+            //        OnRemoteError = ctx =>
+            //        {
+            //            ctx.Response.Redirect("/Account/ExternalLoginCallback?remoteError=" + UrlEncoder.Default.UrlEncode(ctx.Error.Message));
+            //            ctx.HandleResponse();
+            //            return Task.FromResult(0);
+            //        }
+            //    };
+            //});
+            //app.UseMicrosoftAccountAuthentication(options =>
+            //{
+            //    options.ClientId = Configuration["Authentication:MicrosoftAccount:ClientId"];
+            //    options.ClientSecret = Configuration["Authentication:MicrosoftAccount:ClientSecret"];
+            //});
+            //app.UseTwitterAuthentication(options =>
+            //{
+            //    options.ConsumerKey = Configuration["Authentication:Twitter:ConsumerKey"];
+            //    options.ConsumerSecret = Configuration["Authentication:Twitter:ConsumerSecret"];
+            //});
 
-        // Add MVC to the request pipeline.
-        app.UseMvc(routes =>
+            // Add MVC to the request pipeline.
+            app.UseMvc(routes =>
             {
                 routes.MapRoute(
                     name: "default",
