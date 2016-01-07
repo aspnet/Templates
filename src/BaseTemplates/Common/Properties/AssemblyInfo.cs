be9522774a93f96c331490fc0f0ad2b92e683942
Phil Henning <phil.henning@live.com>
2016-01-07 11:46:11 -0800

    Remove Assembly info properties which are supplied by project.json

--- src/BaseTemplates/Common/Properties/AssemblyInfo.cs
+++ src/BaseTemplates/Common/Properties/AssemblyInfo.cs
@@ -5,14 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("$safeprojectname$")]
-[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("$registeredorganization$")]
 [assembly: AssemblyProduct("$safeprojectname$")]
-[assembly: AssemblyCopyright("Copyright © $registeredorganization$ $year$")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
