b036be4ae7169fd803bf2d82598b8390051d78fe
Phil Henning <phil.henning@live.com>
2016-01-22 09:13:32 -0500

    Move ApplicationDbContext file to Data folder
    Closes #380

--- src/Rules/StarterWeb/IndividualAuth/Data/Migrations/00000000000000_CreateIdentitySchema.Designer.cs
+++ src/Rules/StarterWeb/IndividualAuth/Data/Migrations/00000000000000_CreateIdentitySchema.Designer.cs
@@ -6,6 +6,7 @@ using Microsoft.Data.Entity;
 using Microsoft.Data.Entity.Infrastructure;
 using Microsoft.Data.Entity.Metadata;
 using Microsoft.Data.Entity.Migrations;
+using $safeprojectname$.Data;
 using $safeprojectname$.Models;
 
 namespace $safeprojectname$.Data.Migrations
