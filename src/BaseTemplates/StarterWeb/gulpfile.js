2610b0f6d42d4a8ad52f400910e9df39bba9505a
Phil Henning <phil.henning@live.com>
2016-01-22 15:38:43 -0500

    Revert "Add Gulp watch task for JS and CSS minification"
    Watch task doesn't work in all cases.
    This reverts commit 1c71455cd54ac87f3568b7fc2662b8b2daf6fb28.

--- src/BaseTemplates/StarterWeb/gulpfile.js
+++ src/BaseTemplates/StarterWeb/gulpfile.js
@@ -1,4 +1,4 @@
-﻿/// <binding Clean='clean' ProjectOpened='watch' />
+﻿/// <binding Clean='clean' />
 "use strict";
 
 var gulp = require("gulp"),
@@ -43,8 +43,3 @@ gulp.task("min:css", function () {
 });
 
 gulp.task("min", ["min:js", "min:css"]);
-
-gulp.task("watch", function () {
-    gulp.watch([paths.css, "!" + paths.minCss], ["min:css"]);
-    gulp.watch([paths.js, "!" + paths.minJs], ["min:js"]);
-});
