378fc3ba06cdccb6c2884985c6191f5ace0eda1a
Phil Henning <phil.henning@live.com>
2016-01-27 14:36:31 -0500

    Update password validation error messages in view models
    Closes #393

--- src/Rules/StarterWeb/IndividualAuth/Models/ManageViewModels/ChangePasswordViewModel.cs
+++ src/Rules/StarterWeb/IndividualAuth/Models/ManageViewModels/ChangePasswordViewModel.cs
@@ -14,7 +14,7 @@ namespace $safeprojectname$.Models.ManageViewModels
         public string OldPassword { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "New password")]
         public string NewPassword { get; set; }
