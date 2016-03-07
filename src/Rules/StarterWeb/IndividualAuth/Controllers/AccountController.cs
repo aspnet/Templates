6198f0daae835757a8cac47c4b90fd292cc75efd
Hao Kung <haok@microsoft.com>
2016-03-07 13:58:32 -0800

    React to identity changes

--- src/Rules/StarterWeb/IndividualAuth/Controllers/AccountController.cs
+++ src/Rules/StarterWeb/IndividualAuth/Controllers/AccountController.cs
@@ -187,7 +187,7 @@ namespace $safeprojectname$.Controllers
                 // If the user does not have an account, then ask the user to create an account.
                 ViewData["ReturnUrl"] = returnUrl;
                 ViewData["LoginProvider"] = info.LoginProvider;
-                var email = info.ExternalPrincipal.FindFirstValue(ClaimTypes.Email);
+                var email = info.Principal.FindFirstValue(ClaimTypes.Email);
                 return View("ExternalLoginConfirmation", new ExternalLoginConfirmationViewModel { Email = email });
             }
         }
