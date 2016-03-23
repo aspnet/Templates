da84bc4b92f9469924f738284baa32f6bc2cfd4d
Phil Henning <phil.henning@live.com>
2016-03-23 10:54:28 -0700

    Remove unused namespaces

--- src/Rules/StarterWeb/OrganizationalAuth/Single/Controllers/AccountController.cs
+++ src/Rules/StarterWeb/OrganizationalAuth/Single/Controllers/AccountController.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Principal;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
--- src/Rules/StarterWeb/OrganizationalAuth/Single/Startup.cs
+++ src/Rules/StarterWeb/OrganizationalAuth/Single/Startup.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpOverrides;
