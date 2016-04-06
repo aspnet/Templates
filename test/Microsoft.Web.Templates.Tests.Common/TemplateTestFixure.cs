2ef1d47b5ec74bd12d2c984a905bb48c74e74933
Phil Henning <phil.henning@live.com>
2016-04-06 11:10:42 -0700

    Fix build breaks in test folder

--- test/Microsoft.Web.Templates.Tests.Common/TemplateTestFixure.cs
+++ test/Microsoft.Web.Templates.Tests.Common/TemplateTestFixure.cs
@@ -4,7 +4,10 @@ using System.Net.Http;
 using System.Reflection;
 using System.Runtime.Versioning;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
+using Microsoft.AspNetCore.Mvc.Controllers;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.PlatformAbstractions;
@@ -26,7 +29,8 @@ namespace Microsoft.Web.Templates.Tests
         protected TemplateTestFixure(string relativePath)
         {
             var builder = new WebHostBuilder()
-                .ConfigureServices(serviceCollection => InitializeServices(serviceCollection, relativePath))
+                .UseContentRoot(GetApplicationPath(relativePath))
+                .ConfigureServices(InitializeServices)
                 .UseStartup(typeof(TStartup));
 
             _server = new TestServer(builder);
@@ -55,68 +59,26 @@ namespace Microsoft.Web.Templates.Tests
             return Client;
         }
 
-        protected virtual void InitializeServices(IServiceCollection services, string relativePath)
+        private static string GetApplicationPath(string relativePath)
         {
-            // When an application executes in a regular context, the application base path points to the root
-            // directory where the application is located, for example .../samples/MvcSample.Web. However, when
-            // executing an application as part of a test, the ApplicationBasePath of the IApplicationEnvironment
-            // points to the root folder of the test project.
-            // To compensate, we need to calculate the correct project path and override the application
-            // environment value so that components like the view engine work properly in the context of the test.
             var startupAssembly = typeof(TStartup).GetTypeInfo().Assembly;
             var applicationName = startupAssembly.GetName().Name;
-
-            var applicationEnvironment = PlatformServices.Default.Application;
-
-            var applicationRoot = Path.GetFullPath(Path.Combine(
-               applicationEnvironment.ApplicationBasePath,
-               relativePath,
-               applicationName
-            ));
-
-            services.AddSingleton<IApplicationEnvironment>(
-                new TestApplicationEnvironment(applicationEnvironment, applicationName, applicationRoot));
-
-            // Inject a custom assembly provider. Overrides AddMvc() because that uses TryAdd().
-            var assemblyProvider = new StaticAssemblyProvider();
-            assemblyProvider.CandidateAssemblies.Add(startupAssembly);
-            services.AddSingleton<IAssemblyProvider>(assemblyProvider);
-        }
-    }
-
-    // An application environment that overrides the base path of the original
-    // application environment in order to make it point to the folder of the original web
-    // aaplication so that components like ViewEngines can find views as if they were executing
-    // in a regular context.
-    public class TestApplicationEnvironment : IApplicationEnvironment
-    {
-        private readonly IApplicationEnvironment _original;
-
-        public TestApplicationEnvironment(IApplicationEnvironment original, string name, string basePath)
-        {
-            _original = original;
-            ApplicationName = name;
-            ApplicationBasePath = basePath;
+            var applicationBasePath = PlatformServices.Default.Application.ApplicationBasePath;
+            return Path.GetFullPath(Path.Combine(applicationBasePath, relativePath, applicationName));
         }
 
-        public string ApplicationName { get; }
-
-        public string ApplicationVersion
+        protected virtual void InitializeServices(IServiceCollection services)
         {
-            get
-            {
-                return _original.ApplicationVersion;
-            }
-        }
+            var startupAssembly = typeof(TStartup).GetTypeInfo().Assembly;
 
-        public string ApplicationBasePath { get; }
+            // Inject a custom application part manager. Overrides AddMvcCore() because that uses TryAdd().
+            var manager = new ApplicationPartManager();
+            manager.ApplicationParts.Add(new AssemblyPart(startupAssembly));
 
-        public FrameworkName RuntimeFramework
-        {
-            get
-            {
-                return _original.RuntimeFramework;
-            }
+            manager.FeatureProviders.Add(new ControllerFeatureProvider());
+            manager.FeatureProviders.Add(new ViewComponentFeatureProvider());
+
+            services.AddSingleton(manager);
         }
     }
 }
