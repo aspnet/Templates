cab20ea7df4870ec50ee638e33c836b46b3419e0
Phil Henning <phil.henning@live.com>
2016-04-12 13:37:32 -0700

    Remove unneeeded Microsoft.AspNetCore.HttpOverrides using statements

--- src/BaseTemplates/WebAPI/Startup.cs
+++ src/BaseTemplates/WebAPI/Startup.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
