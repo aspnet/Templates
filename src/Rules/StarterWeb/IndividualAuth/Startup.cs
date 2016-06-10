05fe21a88e4b87f41b7086395cba5da08b95c69e
Phil Henning <phil.henning@live.com>
2016-06-10 11:38:20 -0700

    Revert "Comment out builder.AddUserSecrets temporarily to work around ef tooling bug."
    This reverts commit b401c3831a0011d2e679332ee86ad80d1e3fcc03.

--- src/Rules/StarterWeb/IndividualAuth/Startup.cs
+++ src/Rules/StarterWeb/IndividualAuth/Startup.cs
@@ -24,11 +24,11 @@ namespace $safeprojectname$
                 .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
                 .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
 
-            //if (env.IsDevelopment())
-            //{
-            //    // For more details on using the user secret store see http://go.microsoft.com/fwlink/?LinkID=532709
-            //    builder.AddUserSecrets();
-            //}
+            if (env.IsDevelopment())
+            {
+                // For more details on using the user secret store see http://go.microsoft.com/fwlink/?LinkID=532709
+                builder.AddUserSecrets();
+            }
 
             builder.AddEnvironmentVariables();
             Configuration = builder.Build();
