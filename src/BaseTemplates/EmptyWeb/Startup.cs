f7cb5ba29d1fe992a025b2c1fe2c1e24b9d6de79
Glenn Condron <glennc@microsoft.com>
2016-06-15 15:52:53 -0700

    Add logging and diagnostic pages to empty. (#611)

--- src/BaseTemplates/EmptyWeb/Startup.cs
+++ src/BaseTemplates/EmptyWeb/Startup.cs
@@ -18,8 +18,15 @@ namespace $safeprojectname$
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
             app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Hello World!");
