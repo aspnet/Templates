306fbc6b62c01a55c77399688ae57e11f3f6f359
Troy Dai <troy.dai@outlook.com>
2016-08-15 23:21:08 -0700

    Update AccountController for partial auth case
    Clear the external cookie in Login Action when it exists.

--- src/Rules/StarterWeb/IndividualAuth/Controllers/AccountController.cs
+++ src/Rules/StarterWeb/IndividualAuth/Controllers/AccountController.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using $safeprojectname$.Models;
 using $safeprojectname$.Models.AccountViewModels;
 using $safeprojectname$.Services;
@@ -22,16 +23,19 @@ namespace $safeprojectname$.Controllers
         private readonly IEmailSender _emailSender;
         private readonly ISmsSender _smsSender;
         private readonly ILogger _logger;
+        private readonly string _externalCookieScheme;
 
         public AccountController(
             UserManager<ApplicationUser> userManager,
             SignInManager<ApplicationUser> signInManager,
+            IOptions<IdentityCookieOptions> identityCookieOptions,
             IEmailSender emailSender,
             ISmsSender smsSender,
             ILoggerFactory loggerFactory)
         {
             _userManager = userManager;
             _signInManager = signInManager;
+            _externalCookieScheme = identityCookieOptions.Value.ExternalCookieAuthenticationScheme;
             _emailSender = emailSender;
             _smsSender = smsSender;
             _logger = loggerFactory.CreateLogger<AccountController>();
@@ -43,6 +47,9 @@ namespace $safeprojectname$.Controllers
         [AllowAnonymous]
         public IActionResult Login(string returnUrl = null)
         {
+            // Clear the existing external cookie to ensure a clean login process
+            await HttpContext.Authentication.SignOutAsync(_externalCookieScheme);
+
             ViewData["ReturnUrl"] = returnUrl;
             return View();
         }
