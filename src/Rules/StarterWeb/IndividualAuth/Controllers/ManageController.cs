6edd4e3d52756ab8f4c9f352cb9e3830b2b1bb9d
Jonathan <vanillajonathan@users.noreply.github.com>
2016-09-27 15:03:35 +0200

    Recommend https by a commented out attribute

--- src/Rules/StarterWeb/IndividualAuth/Controllers/ManageController.cs
+++ src/Rules/StarterWeb/IndividualAuth/Controllers/ManageController.cs
@@ -13,6 +13,7 @@ using $safeprojectname$.Services;
 namespace $safeprojectname$.Controllers
 {
     [Authorize]
+    //[RequireHttps]
     public class ManageController : Controller
     {
         private readonly UserManager<ApplicationUser> _userManager;
