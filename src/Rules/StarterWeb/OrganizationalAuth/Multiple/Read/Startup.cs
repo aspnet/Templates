6a06e197ecc7c2162a3f9f321c344e64656ef8cc
Christian Weiss <c.weiss@c3-ls.com>
2016-10-14 22:35:09 +0200

    Removes environment specific appsettings files

--- src/Rules/StarterWeb/OrganizationalAuth/Multiple/Read/Startup.cs
+++ src/Rules/StarterWeb/OrganizationalAuth/Multiple/Read/Startup.cs
@@ -20,8 +20,7 @@ namespace $safeprojectname$
         {
             var builder = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
 
             if (env.IsDevelopment())
             {
