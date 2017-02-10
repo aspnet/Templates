460d27ca51257fb047ccaabe727cd7aac7b088cb
Phil Henning <phenning@microsoft.com>
2017-02-10 12:54:40 -0800

    Use https instead of http in some fwlinks

--- src/Rules/StarterWeb/IndividualAuth/Startup.cs
+++ src/Rules/StarterWeb/IndividualAuth/Startup.cs
@@ -26,7 +26,7 @@ namespace $safeprojectname$
 
             if (env.IsDevelopment())
             {
-                // For more details on using the user secret store see http://go.microsoft.com/fwlink/?LinkID=532709
+                // For more details on using the user secret store see https://go.microsoft.com/fwlink/?LinkID=532709
                 builder.AddUserSecrets<Startup>();
             }
 
@@ -75,7 +75,7 @@ namespace $safeprojectname$
 
             app.UseIdentity();
 
-            // Add external authentication middleware below. To configure them please see http://go.microsoft.com/fwlink/?LinkID=532715
+            // Add external authentication middleware below. To configure them please see https://go.microsoft.com/fwlink/?LinkID=532715
 
             app.UseMvc(routes =>
             {
--- src/Rules/StarterWeb/OrganizationalAuth/Multiple/NoRead/Startup.cs
+++ src/Rules/StarterWeb/OrganizationalAuth/Multiple/NoRead/Startup.cs
@@ -24,7 +24,7 @@ namespace $safeprojectname$
 
             if (env.IsDevelopment())
             {
-                // For more details on using the user secret store see http://go.microsoft.com/fwlink/?LinkID=532709
+                // For more details on using the user secret store see https://go.microsoft.com/fwlink/?LinkID=532709
                 builder.AddUserSecrets<Startup>();
             }
             builder.AddEnvironmentVariables();
